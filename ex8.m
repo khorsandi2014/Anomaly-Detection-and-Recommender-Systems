ac3aaea627b5552b1bc5fdb0503b2e25ef1cadb0
khorsandi2014 <rahman.khorsandi@gmail.com>
2016-04-29 17:13:48 -0500

    Update ex8.m

--- ex8.m
+++ ex8.m
@@ -1,31 +1,6 @@
-%% Machine Learning Online Class
-%  Exercise 8 | Anomaly Detection and Collaborative Filtering
-%
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  exercise. You will need to complete the following functions:
-%
-%     estimateGaussian.m
-%     selectThreshold.m
-%     cofiCostFunc.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
 
 %% Initialization
 clear ; close all; clc
-
-%% ================== Part 1: Load Example Dataset  ===================
-%  We start this exercise by using a small dataset that is easy to
-%  visualize.
-%
-%  Our example case consists of 2 network server statistics across
-%  several machines: the latency and throughput of each machine.
-%  This exercise will help us find possibly faulty (or very fast) machines.
-%
 
 fprintf('Visualizing example dataset for outlier detection.\n\n');
 
@@ -40,17 +15,7 @@ xlabel('Latency (ms)');
 ylabel('Throughput (mb/s)');
 
 fprintf('Program paused. Press enter to continue.\n');
-% pause
 
-
-%% ================== Part 2: Estimate the dataset statistics ===================
-%  For this exercise, we assume a Gaussian distribution for the dataset.
-%
-%  We first estimate the parameters of our assumed Gaussian distribution, 
-%  then compute the probabilities for each of the points and then visualize 
-%  both the overall distribution and where each of the points falls in 
-%  terms of that distribution.
-%
 fprintf('Visualizing Gaussian fit.\n\n');
 
 %  Estimate my and sigma2
@@ -67,11 +32,6 @@ ylabel('Throughput (mb/s)');
 
 fprintf('Program paused. Press enter to continue.\n');
 % pause;
-
-%% ================== Part 3: Find Outliers ===================
-%  Now you will find a good epsilon threshold using a cross-validation set
-%  probabilities given the estimated Gaussian distribution
-% 
 
 pval = multivariateGaussian(Xval, mu, sigma2);
 
@@ -90,12 +50,6 @@ hold off
 
 fprintf('Program paused. Press enter to continue.\n');
 % pause;
-
-%% ================== Part 4: Multidimensional Outliers ===================
-%  We will now use the code from the previous part and apply it to a 
-%  harder problem in which more features describe each datapoint and only 
-%  some features indicate whether a point is an outlier.
-%
 
 %  Loads the second dataset. You should now have the
 %  variables X, Xval, yval in your environment
